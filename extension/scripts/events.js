594da7def8ecf3f0c40f3786a5247dd96a9b1462
Jomar Tigcal <jomar@tigcal.com>
2022-11-28 18:42:27 -0800

    Fetching list of events from gdg.community.dev instead of from Meetup API

--- extension/scripts/events.js
+++ extension/scripts/events.js
@@ -3,16 +3,16 @@ $( document ).ready(function() {
 });
 
 function getEvents() {
-	$.get("https://api.meetup.com/2/events?&sign=true&photo-host=public&page=50&status=past,upcoming&group_urlname=gdgphilippines", function(data, status) {
+	$.get("https://gdg.community.dev/api/event?chapter=605", function(data, status) {
 		displayEvents(data.results)
 	})
 }
 
 function displayEvents(events) {
 	var eventString = ""
-	for(i=events.length-1; i>0; i--) {
+	for(i=0; i<events.length; i++) {
 		var event = events[i]
-		eventString += "<br /><br /><a href='" + event.event_url + "' target='_blank'>" + event.name + "</a>"
+		eventString += "<br /><br /><a href='" + event.url + "' target='_blank'>" + event.title + "</a>"
 	}
 
 	document.getElementById("events").innerHTML = eventString
